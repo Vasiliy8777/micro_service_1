7c3b1737849df752c525d51d7d992bedab094e4e
Vasiliy <exs777@mail.ru>
2025-12-27 19:31:09 +0700

    commit init

---
+++ eureca-server/src/main/java/org/mc1/eurecaserver/EurecaServerApplication.java
@@ -0,0 +1,13 @@
+package org.mc1.eurecaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurecaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurecaServerApplication.class, args);
+    }
+
+}
